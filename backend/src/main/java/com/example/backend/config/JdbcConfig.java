482a83a5dc8c212aa586b2e6c9927379ce9dffca
Yuno04 <121325560+Yuno04@users.noreply.github.com>
2025-10-29 13:38:57 +0700

    Complete backend V1 + Docker + maven + java

---
+++ backend/src/main/java/com/example/backend/config/JdbcConfig.java
@@ -0,0 +1,14 @@
+package main.java.com.example.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import javax.sql.DataSource;
+
+@Configuration
+public class JdbcConfig {
+  @Bean
+  JdbcClient jdbcClient(DataSource dataSource) {
+    return JdbcClient.create(dataSource);
+  }
+}
